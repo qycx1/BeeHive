32ad40cf35dfee78b33865fbb180acde3b062f38
Dexter Facelo <Dexter101306@gmail.com>
2025-08-23 10:52:40 +0800

    feat(auth): enforce authentication rules

---
+++ src/main/java/com/hive/modules/authentication/domain/service/AddAuthentication.java
@@ -0,0 +1,39 @@
+package com.hive.modules.authentication.domain.service;
+
+import com.hive.modules.authentication.domain.factory.AuthFactory;
+import com.hive.modules.authentication.domain.model.Auth;
+import com.hive.modules.authentication.domain.repository.AuthRepository;
+import com.hive.shared.result.Result;
+
+
+public final class AddAuthentication {
+
+    private final AuthFactory factory;
+    private final AuthRepository repository;
+
+    public AddAuthentication(AuthFactory factory, AuthRepository repository) {
+        this.factory = factory;
+        this.repository = repository;
+    }
+
+    public Result<Auth> add(String userId, String username, String password) {
+
+        if(username.length() < 4) {
+            return Result.fail("Username must not be less than 4 characters.");
+        }
+
+        if(password.length() < 8) {
+            return Result.fail("Password must be more than 8 characters long.");
+        }
+
+        if(repository.findByUsername(username).isPresent()) {
+            return Result.fail("Username already taken.");
+        }
+
+        Auth authentication = factory.create(userId, username, password);
+        repository.save(authentication);
+
+        return Result.ok(authentication);
+    }
+}
+
