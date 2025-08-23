b24e400d4d457e650b461943a177bd9f88e09ad0
Dexter Facelo <Dexter101306@gmail.com>
2025-08-23 12:15:58 +0800

    feat: import BCrypt and add BCryptHasher

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.postgresql:postgresql:42.7.7")
+    implementation("org.mindrot:jbcrypt:0.4")
 }
 
 tasks.test {
