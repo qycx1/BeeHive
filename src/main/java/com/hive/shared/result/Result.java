6a4745b7e528884ce8431de2e6605a8c58b9b72e
Dexter Facelo <Dexter101306@gmail.com>
2025-08-23 14:27:17 +0800

    refactor(Result): make errorMessage generic

--- src/main/java/com/hive/shared/result/Result.java
+++ src/main/java/com/hive/shared/result/Result.java
@@ -1,17 +1,14 @@
 package com.hive.shared.result;
 
-public record Result<T>(
-    boolean success,
-    T data,
-    String errorMessage
-)
+public record Result<T,E>(boolean success, T data, E errorMessage)
 {
 
-    public static <T> Result<T> ok(T data) {
+    public static <T,E> Result<T,E> ok(T data) {
         return new Result<>(true, data, null);
     }
 
-    public static <T> Result<T> fail(String errorMessage) {
+
+    public static <T,E> Result<T,E> fail(E errorMessage) {
         return new Result<>(false, null, errorMessage);
     }
 
