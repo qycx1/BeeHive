e4331b27a61007ecf1f06fd68f08eebd9965e28d
Dexter Facelo <Dexter101306@gmail.com>
2025-08-23 09:42:21 +0800

    chore: add Auth Repository interface

---
+++ src/main/java/com/hive/modules/authentication/domain/repository/AuthRepository.java
@@ -0,0 +1,13 @@
+package com.hive.modules.authentication.domain.repository;
+
+import com.hive.modules.authentication.domain.model.Auth;
+
+import java.util.Optional;
+
+public interface AuthRepository {
+
+    Optional<Auth> findById(String userId);
+    Optional<Auth> findByUsername(String username);
+    void save(Auth auth);
+    void delete(Auth auth);
+}
