f38c3080ca7c17ec5a7ac9c6c40a1ff1e3f58ff7
Dexter Facelo <Dexter101306@gmail.com>
2025-09-12 15:51:51 +0800

    refactor: add Role in UserProfileResult parameter

--- src/main/java/com/hive/modules/authentication/application/response/UserProfileResult.java
+++ src/main/java/com/hive/modules/authentication/application/response/UserProfileResult.java
@@ -1,7 +1,9 @@
 package com.hive.modules.authentication.application.response;
 
+import com.hive.modules.users.domain.model.Roles;
+
 import java.time.LocalDate;
 import java.util.Set;
 
-public record UserProfileResult (String userId,String authId ,String firstName, String lastName, String email, Set<String> addresses, String phoneNumber, LocalDate birthday){
+public record UserProfileResult (String userId,String authId ,String firstName, String lastName, String email, Set<String> addresses, String phoneNumber, LocalDate birthday, Roles roles){
 }
