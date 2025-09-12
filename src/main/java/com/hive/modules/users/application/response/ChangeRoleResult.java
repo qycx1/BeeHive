f820d04af787e95ed64a5d22f99fa8d02cbf6142
Dexter Facelo <Dexter101306@gmail.com>
2025-09-12 16:08:04 +0800

    refactor(ChangeRoleResult): add message parameter

--- src/main/java/com/hive/modules/users/application/response/ChangeRoleResult.java
+++ src/main/java/com/hive/modules/users/application/response/ChangeRoleResult.java
@@ -5,8 +5,18 @@ import com.hive.modules.users.domain.model.User;
 import java.util.Optional;
 
 public enum ChangeRoleResult {
-    USERID_NULL,
-    ROLE_NULL,
-    USER_ID_NOT_FOUND,
-    CHANGE_ROLE_SUCCESS;
+    USERID_NULL("Invalid User ID"),
+    ROLE_NULL("Invalid Role"),
+    USER_ID_NOT_FOUND("User ID is not found."),
+    CHANGE_ROLE_SUCCESS("Successfully changed role");
+
+    private final String message;
+
+    ChangeRoleResult(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
