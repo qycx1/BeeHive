2d5bebaad5428f4e24900ce33f61e6d129d5b807
Dexter Facelo <Dexter101306@gmail.com>
2025-09-12 15:58:03 +0800

    feat(repository): add update method to UserRepository interface

--- src/main/java/com/hive/modules/users/domain/repository/UserRepository.java
+++ src/main/java/com/hive/modules/users/domain/repository/UserRepository.java
@@ -2,6 +2,10 @@ package com.hive.modules.users.domain.repository;
 
 import com.hive.modules.users.domain.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository {
+    Optional<User> findByUserId(String userId);
     void save(User user);
+    void update(User user);
 }
