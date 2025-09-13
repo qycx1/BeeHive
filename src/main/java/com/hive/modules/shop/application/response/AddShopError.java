b5d001cd008797b5ee448c659a06d19e00bacba1
Dexter Facelo <Dexter101306@gmail.com>
2025-09-13 22:02:19 +0800

    refactor(shop): add USER_ALREADY_HAS_SHOP enum value

--- src/main/java/com/hive/modules/shop/application/response/AddShopError.java
+++ src/main/java/com/hive/modules/shop/application/response/AddShopError.java
@@ -5,6 +5,7 @@ public enum AddShopError {
     MISSING_SHOP_NAME("Shop name is required"),
     MISSING_DESCRIPTION("Description is required"),
     SHOP_NAME_ALREADY_EXISTS("Shop name already exists for this user"),
+    USER_ALREADY_HAS_SHOP("User already owns a shop"),
     UNKNOWN_ERROR("An unknown error occurred");
 
     private final String defaultMessage;
